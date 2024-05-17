b217e303ac713bc177a3b9478f64120f1eda384f
AttePiltonen <79204283+AttePiltonen@users.noreply.github.com>
2024-05-17 14:14:35 -0400

    Solve ex2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str, factor) {
+    if (factor < 0) {
+        return "ERROR"
+    }
+    return str.repeat(factor)
 };
 
 // Do not edit below this line
