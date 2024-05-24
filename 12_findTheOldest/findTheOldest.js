154e69c08f71fbdece586d14c33147d865ea540f
AttePiltonen <79204283+AttePiltonen@users.noreply.github.com>
2024-05-24 17:11:39 -0400

    Solve ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people = people.map(person => {
+        let age;
+        if (person.yearOfDeath) {
+            age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            age = new Date().getFullYear() - person.yearOfBirth;
+        }
+        return {
+            ...person,
+            age // Use the shorthand property name for age: age
+        };
+    }).sort((a, b) => b.age - a.age);
+    
+    return people[0]; // Return the entire object of the oldest person
 };
 
 // Do not edit below this line
