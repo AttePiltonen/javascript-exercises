4f5661c80e4ff60886bd111f3e1285369335f21f
AttePiltonen <79204283+AttePiltonen@users.noreply.github.com>
2024-05-24 14:54:41 -0400

    Solve ex9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    let left = 0;
+    let right = word.length - 1
+    while (left < right) {
+        if (word[left] !== word[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
 };
 
 // Do not edit below this line
